e795d83e1d70042e76629627d30472cb98d40a9b
maxvonlancaster <windchange7@gmail.com>
2023-02-16 13:39:26 +0200

    labs progress

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.btn-warning').tooltip({
+    title: "In progress: please, make it work properly",
+    placement: "left"
+});
+
+$('.btn-success').tooltip({
+    title: "Great Job!",
+    placement: "left"
+});
