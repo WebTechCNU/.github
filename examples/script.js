d61553538be51b2b7618c020195f16db5dc8bf94
maxvonlancaster <windchange7@gmail.com>
2024-03-10 23:45:31 +0200

    upd

--- examples/script.js
+++ examples/script.js
@@ -1,9 +1,18 @@
-let points = [
-    {name : "Sol", coordinates: [50, 50], class: 0.5, 
-        description: "Sol system" },
-    {name : "Alpha Centauri", coordinates: [55, 52], class: 0.4, 
-        description: "The closest system to us, home to evil trisolarians" },
-    {name : "Shedar", coordinates: [25, 25], class: 0.6, 
-        description: "Alpha Cassiopei" }
+let planets = [
+    {id: 1, name: "Earth", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 2, name: "Vulcan", sector: 0, class: 1, size: 0.8, population: 6, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 3, name: "Klingon", sector: 0, class: 1, size: 1.2, population: 11, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 4, name: "Romulus", sector: 0, class: 1, size: 1.2, population: 19, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 5, name: "Andor", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 6, name: "Tellar", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 7, name: "Bolarus", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 9, name: "Cardassia", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 10, name: "Bajor", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 11, name: "Ferenginar", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 12, name: "Breen", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 13, name: "Tholia", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 14, name: "Gorn Prime", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
+    {id: 15, name: "Tama", sector: 0, class: 1, size: 1, population: 8, coordinates: [0, 0], colour: [0,0,255]},
 ]
+
 
