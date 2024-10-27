6f96095beb1ed0f3047917f5de18e3e4484e0116
Vasyl Melnyk <windchange7@gmail.com>
2024-10-28 00:42:25 +0200

    upd

--- 2425/students/stud_oit.js
+++ 2425/students/stud_oit.js
@@ -10,10 +10,10 @@ let students = `[
         [0,"","",""],
         [0,"","",""]]},
     {"name":"Барбуца Микола","points":[
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.8,"https://github.com/NicuJun/Lab-1","","Late submission"],
+        [1,"https://github.com/NicuJun/Lab_2","",""],
+        [1,"https://github.com/NicuJun/Lab-3","",""],
+        [1,"https://github.com/NicuJun/Lab_4","",""]]},
     {"name":"Боднарюк Олександра","points":[
         [1,"https://github.com/Oleksandra851/Lab1","https://oleksandra851.github.io/Lab1/index.html",""],
         [1,"https://github.com/Oleksandra851/lab.2","https://oleksandra851.github.io/lab.2/Lab.2/",""],
@@ -22,46 +22,46 @@ let students = `[
     {"name":"Бойко Аліна","points":[
         [1,"https://github.com/AlinaIllivna/Lab1_OIT","https://alinaillivna.github.io/Lab1_OIT/main_page.html",""],
         [1,"https://github.com/AlinaIllivna/Lab2","https://alinaillivna.github.io/Lab2/main_page.html",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [1,"https://github.com/AlinaIllivna/Laboratorna3","https://alinaillivna.github.io/Laboratorna3/projects.html",""],
+        [1,"https://github.com/AlinaIllivna/Lab4","https://alinaillivna.github.io/Lab4/main_page.html",""]]},
     {"name":"Бучек Іван","points":[
         [0,"","",""],
         [0,"","",""],
         [0,"","",""],
         [0,"","",""]]},     
     {"name":"Волик Таміла","points":[
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.8,"https://github.com/pleasurekiller2015/lab1","https://pleasurekiller2015.github.io/lab1/","Late submission"],
+        [1,"https://github.com/pleasurekiller2015/lab2_1/tree/main","https://pleasurekiller2015.github.io/lab2_1/",""],
+        [1,"https://github.com/pleasurekiller2015/lab3","https://pleasurekiller2015.github.io/lab3/",""],
+        [0.8,"https://github.com/pleasurekiller2015/laba4_2","https://pleasurekiller2015.github.io/laba4_2/","No bootstrap"]]},
     {"name":"Діліон Іван-Владислав","points":[
         [1,"https://github.com/dzidzio/lab1","https://dzidzio.github.io/lab1/","","!!!"],
         [1,"https://github.com/dzidzio/OIT_lab2","https://dzidzio.github.io/OIT_lab2/code/index.html",""],
         [1,"https://github.com/dzidzio/lab3","https://dzidzio.github.io/lab3/",""],
-        [0,"","",""]]},
+        [1,"https://github.com/dzidzio/lab4","https://dzidzio.github.io/lab4/",""]]},
     {"name":"Дубчак Богдан","points":[
         [1,"https://github.com/Dub0g/oit_lab_1","https://dub0g.github.io/oit_lab_1/",""],
         [1,"https://github.com/Dub0g/lab_2","https://dub0g.github.io/lab_2/",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [1,"https://github.com/Dub0g/lab_3","https://dub0g.github.io/lab_3/front-end/#top",""],
+        [1,"https://github.com/Dub0g/lab_4","https://dub0g.github.io/lab_4/front-end/index.html#",""]]},
     {"name":"Іванов Микита","points":[
         [0,"","",""],
         [0,"","",""],
         [0,"","",""],
         [0,"","",""]]},
     {"name":"Кіцул Сергій","points":[
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.5,"https://github.com/SerhiiKitsul999/MIMIMI/blob/main/index.html","","Late submition, no deploy"],
+        [0.5,"https://github.com/SerhiiKitsul999/201-A-2-LAB-","","No deploy"],
+        [0.5,"https://github.com/SerhiiKitsul999/201-A-LABA-3-","","No deploy"],
+        [0.5,"https://github.com/SerhiiKitsul999/201-A-4-LAB-40-","","No deploy"]]},
     {"name":"Косован Іван-Валентин","points":[
         [0.5,"https://github.com/valentyn-oss/oit-lab1","https://github.com/valentyn-oss/oit-lab1","No deploy link"],
         [0,"","",""],
         [0,"","",""],
         [0,"","",""]]},   
     {"name":"Лупул Михайло","points":[
-        [0,"","",""],
-        [0,"","",""],
+        [0.8,"https://github.com/msht322/lab1","https://msht322.github.io/lab1/index.html","Late submission"],
+        [1,"https://github.com/msht322/lab2","https://msht322.github.io/lab2/",""],
         [0,"","",""],
         [0,"","",""]]},
     {"name":"Миленко Юрій","points":[
@@ -77,8 +77,8 @@ let students = `[
     {"name":"Ігор Яцько","points":[
         [1,"https://github.com/Nagat0-0/lab_1_OIT","https://nagat0-0.github.io/lab_1_OIT/index.html",""],
         [1,"https://github.com/Nagat0-0/lab2_oit","https://nagat0-0.github.io/lab2_oit/",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [1,"https://github.com/Nagat0-0/lab3_oit","https://nagat0-0.github.io/lab3_oit/",""],
+        [0.8,"https://github.com/Nagat0-0/lab4_oit","https://nagat0-0.github.io/lab4_oit/","No modal window"]]},
     {"name":"Батуєва Анастасія (211A)","points":[
         [0,"","",""],
         [0,"","",""],
@@ -98,7 +98,7 @@ let students = `[
         [1,"https://github.com/oksankah/public_html","https://oksankah.github.io/public_html/","!!!"],
         [1,"https://github.com/oksankah/oitlab2","https://github.com/oksankah/oitlab2","No deploy, but presented offline ✅"],
         [1,"https://github.com/oksankah/oitlab3","","No deploy, but presented offline ✅"],
-        [0,"","",""]]},
+        [1,"https://github.com/oksankah/oitlab4","https://oksankah.github.io/oitlab4/oitlab/",""]]},
     {"name":"Голбан Уляна","points":[
         [1,"https://github.com/golbanulyana/Laboratory-1/","https://golbanulyana.github.io/Laboratory-1/homepage.html",""],
         [1,"https://github.com/GolbanUlyana/lab2.","https://golbanulyana.github.io/lab2./",""],
@@ -120,15 +120,15 @@ let students = `[
         [0,"","",""],
         [0,"","",""]]},
     {"name":"Дубчак Олександр","points":[
+        [0.5,"https://github.com/illusions333/lab1","","No deploy, no offline presentation"],
         [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},   
+        [0.5,"https://github.com/illusions333/lab3","","No deploy, no offline presentation"],
+        [0.8,"https://github.com/illusions333/lab4","https://illusions333.github.io/lab4/","No bootstrap"]]},   
     {"name":"Заєць Марія","points":[
         [1,"https://github.com/zaietsmary/lab1","https://zaietsmary.github.io/lab1/",""],
         [1,"https://github.com/zaietsmary/lab2","https://zaietsmary.github.io/lab2/",""],
         [1,"https://github.com/zaietsmary/lab3","https://zaietsmary.github.io/lab3/",""],
-        [0,"","",""]]},
+        [1,"https://github.com/zaietsmary/lab4","https://zaietsmary.github.io/lab4/",""]]},
     {"name":"Ілюк Богдан","points":[
         [0,"","",""],
         [0,"","",""],
@@ -138,17 +138,17 @@ let students = `[
         [1,"https://github.com/nastyaklypak/lab_1","https://nastyaklypak.github.io/lab_1/","", "!!!"],
         [1,"https://github.com/nastyaklypak/lab_2","https://nastyaklypak.github.io/lab_2/",""],
         [1,"https://github.com/nastyaklypak/lab3","https://github.com/nastyaklypak/lab3","No deploy, but presented offline ✅"],
-        [0.8,"https://github.com/nastyaklypak/lab_4","","No deploy, but presented offline ✅"]]},
+        [1,"https://github.com/nastyaklypak/lab_4","https://nastyaklypak.github.io/lab_4/",""]]},
     {"name":"Коба Сергій","points":[
         [1,"https://github.com/qenzai/laba-2","https://qenzai.github.io/laba-2/",""],
         [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.8,"https://github.com/qenzai/laba-3","https://qenzai.github.io/laba-3/index.html","Scroll-up button is present when on top of page; Questionary form is not ideal"],
+        [0.8,"https://github.com/qenzai/laba-4","https://qenzai.github.io/laba-4/?","No modal window"]]},
     {"name":"Ковальчук Роман","points":[
         [1,"https://github.com/r0mank0valchuk/Lab_1","https://r0mank0valchuk.github.io/Lab_1/index.html",""],
         [1,"https://github.com/r0mank0valchuk/Lab_2","https://r0mank0valchuk.github.io/Lab_2/",""],
         [1,"https://github.com/r0mank0valchuk/lab3","https://r0mank0valchuk.github.io/lab3/#top",""],
-        [0,"","",""]]},
+        [1,"https://github.com/r0mank0valchuk/Lab_4","https://r0mank0valchuk.github.io/Lab_4/",""]]},
     {"name":"Антонов Олександр (208)","points":[
         [0,"","",""],
         [0,"","",""],
@@ -160,10 +160,10 @@ let students = `[
         [0,"","",""],
         [0,"","",""]]},
     {"name":"Воєвідко Олексій","points":[
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.8,"https://github.com/nothing-real/lab-1","https://nothing-real.github.io/lab-1/","Late submission"],
+        [0.8,"https://github.com/nothing-real/lab-2","https://nothing-real.github.io/lab-2/","No fonts"],
+        [0.8,"https://github.com/nothing-real/lab-3","https://nothing-real.github.io/lab-3/","No scroll-up button"],
+        [0.8,"https://github.com/nothing-real/lab-4","https://nothing-real.github.io/lab-4/","No adaptive design"]]},
     {"name":"Глушко Ігор","points":[
         [0,"","",""],
         [0,"","",""],
@@ -178,13 +178,13 @@ let students = `[
     {"name":"Дашкевич Богдан","points":[
         [0.5,"https://github.com/bogdan23321/Laboratory1","","No deploy, no offline presentation"],
         [0.5,"https://github.com/bogdan23321/lab2","","No deploy, no offline presentation"],
-        [0,"","",""],
+        [0.5,"https://github.com/bogdan23321/Lab3/tree/main","","No deploy, no offline presentation"],
         [0,"","",""]]},
     {"name":"Діліон Євгенія","points":[
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.8,"","https://eugenia-dilion.github.io/lab1/","Late submission"],
+        [0.5,"","https://eugenia-dilion.github.io/lab2/","No root variable, no fonts"],
+        [1,"","https://eugenia-dilion.github.io/lab3/",""],
+        [0.5,"","https://eugenia-dilion.github.io/lab4/","No adaptive design, no bootstrap, no modal window"]]},
     {"name":"Казакова Ольга","points":[
         [0,"","",""],
         [0,"","",""],
@@ -209,12 +209,12 @@ let students = `[
         [1,"https://github.com/Valeriia1Lera/project1","https://github.com/Valeriia1Lera/project1","No deploy"],
         [0.8,"https://github.com/Valeriia1Lera/lab-2","","No deploy, no root variables"],
         [0.5,"https://github.com/Valeriia1Lera/lab3","","No deploy"],
-        [0,"","",""]]},
+        [0.5,"https://github.com/Valeriia1Lera/lab4","","No deploy, no offline presentation"]]},
     {"name":"Сокровольська Надія","points":[
         [1,"https://github.com/demonishko/html-css-web-app","https://demonishko.github.io/html-css-web-app/","Broken deploy, but lab presented offline, accepted ✅"],
         [1,"https://github.com/demonishko/html-css-web-app","https://demonishko.github.io/html-css-web-app/","Broken deploy, but lab presented offline, accepted ✅"],
         [1,"https://github.com/demonishko/html-css-web-app","https://demonishko.github.io/html-css-web-app/","Broken deploy, but lab presented offline, accepted ✅"],
-        [0,"","",""]]},
+        [1,"https://github.com/demonishko/html-css-web-app.git","https://6716b1dec3a49ac12de6cef4--timely-phoenix-73fd78.netlify.app/",""]]},
     {"name":"Соловйов Володимир","points":[
         [0.8,"https://github.com/SoloviovVolodymyr/Laboratory-1","https://soloviovvolodymyr.github.io/Laboratory-1/Laboratory/","Hyperlinks not working"],
         [0.8,"https://github.com/SoloviovVolodymyr/Laboratory2","","No deploy, otherwise all ok"],
@@ -229,7 +229,7 @@ let students = `[
         [1,"https://github.com/yuliiatymchuk07/lab1","https://yuliiatymchuk07.github.io/lab1/",""],
         [1,"https://github.com/yuliiatymchuk07/--2","https://yuliiatymchuk07.github.io/--2/",""],
         [1,"https://github.com/yuliiatymchuk07/lab3-1-","https://yuliiatymchuk07.github.io/lab3-1-/",""],
-        [0,"","",""]]},
+        [1,"https://github.com/yuliiatymchuk07/lab4","https://yuliiatymchuk07.github.io/lab4/%D0%AE%D0%BB%D1%8F%204%20%D0%BB%D0%B0%D0%B1%D0%B0%20%D0%B7%20%D0%9E%D0%86%D0%A2%20(2)/%D0%AE%D0%BB%D1%8F%204%20%D0%BB%D0%B0%D0%B1%D0%B0%20%D0%B7%20%D0%9E%D0%86%D0%A2/",""]]},
     {"name":"Тихович Михайло","points":[
         [0,"","",""],
         [0,"","",""],
@@ -238,20 +238,20 @@ let students = `[
     {"name":"Ткач Микола","points":[
         [1,"https://github.com/TkachMykola/LAB_2_OIT","https://tkachmykola.github.io/LAB_2_OIT/Country.html",""],
         [1,"https://github.com/TkachMykola/LAB_2_OIT","https://tkachmykola.github.io/LAB_2_OIT/Country.html",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.8,"https://github.com/TkachMykola/lab3_OIT","https://tkachmykola.github.io/lab3_OIT/","No scroll up"],
+        [0.8,"https://github.com/TkachMykola/lab4_OIT","https://tkachmykola.github.io/lab4_OIT/","No feedback section"]]},
     {"name":"Шевчук Іван","points":[
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""],
-        [0,"","",""]]},
+        [0.5,"https://github.com/IvanShevchuk208/Lab1.git","",""],
+        [0.5,"https://github.com/IvanShevchuk208/Lab2.git","",""],
+        [0.5,"https://github.com/IvanShevchuk208/Lab3.git","",""],
+        [0.8,"https://github.com/IvanShevchuk208/Lab4.git","https://steady-figolla-18bac5.netlify.app/","No adaptive design; no bootstrap"]]},
     {"name":"Якубовський Максим","points":[
         [0,"","",""],
         [1,"https://github.com/Yakubovskyimaksym/Labaragory-2","https://yakubovskyimaksym.github.io/Labaragory-2/",""],
         [0,"","",""],
         [0,"","",""]]},
     {"name":"Яремчук Софія","points":[
-        [0,"","",""],
+        [0.8,"https://github.com/Yaremchukk/lab1","https://yaremchukk.github.io/lab1/%D1%81%D0%BE%D1%84%D1%96%D1%8F/#","Late submition"],
         [1,"https://github.com/Yakubovskyimaksym/Labaragory-2","https://yakubovskyimaksym.github.io/Labaragory-2/",""],
         [0,"","",""],
         [0,"","",""]]}
